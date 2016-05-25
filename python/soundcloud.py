232c1eb0eed840bc3bbb29c58514b86e606b1d3e
Vivek Menon <mail@vkmn.xyz>
2016-05-25 15:08:59 -0400

    More housekeeping

--- python/soundcloud.py
+++ python/soundcloud.py
@@ -5,24 +5,31 @@ from selenium import webdriver
 import random
 import time
 
-URL = "https://soundcloud.com/itsekke/glory-in-the-highest-prod-ryan-little"
-
-
-def Player(URL):
+def Player(URL, loops, mintime, maxtime):
     # Load a Chrome Browser and go to the URL
+    print('Loading browser...')
     browser = webdriver.Chrome()
     browser.get(URL)
-    for loop in range(1, 100):
-        # Get play count
-        playCounter = browser.find_element_by_css_selector('#content > div > div.l-listen-wrapper > div.l-about-main > div > div:nth-child(1) > div > div > div.listenEngagement__footer.sc-clearfix > div.listenEngagement__extras > div > ul > li:nth-child(1) > span > span:nth-child(2)')
-        play = playCounter.text
+    time.sleep(5)
+    title = browser.title
+    print('Beginning loops for %s:' % (title))
+    play = []
+    for loop in range(1, loops):
+        try:
+            playCounter = browser.find_element_by_css_selector('#content > div > div.l-listen-wrapper > div.l-about-main > div > div:nth-child(1) > div > div > div.listenEngagement__footer.sc-clearfix > div.listenEngagement__extras > div > ul')
+            play.append(playCounter.text[0:3])
+            print("Current play count is %s." %(play[-1]))
+        except:
+            continue
         # Wait
-        time.sleep(random.randrange(30, 15, 1))
-        # Quit the browser instance
+        time.sleep(random.randrange(mintime, maxtime, 1))
+        # Refresh/quit browser instance
         browser.refresh()
         # Metrics
         print("This is loop number %s." %(loop))
-        print("Current play count is %s." %(play))
+    print('Loops for %s have increased by %d.' %(title, loops-1))
+    print('Plays for %s have increased by %d.' %(title, int(play[-1])-int(play[0])))
+    #End Session
+    browser.quit()
 
-Player(URL)
- 
+Player("https://soundcloud.com/itsekke/glory-in-the-highest-prod-ryan-little", 300, 5, 10)
