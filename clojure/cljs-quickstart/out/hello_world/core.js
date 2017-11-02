3f0f4597c8aaa52892d8880f1a02874df7ba8805
Vivek Menon <mail@vvkmnn.xyz>
2017-11-02 19:20:26 -0400

    FEAT(clojure): ClojureScript quick start.

---
+++ clojure/cljs-quickstart/out/hello_world/core.js
@@ -0,0 +1,7 @@
+// Compiled by ClojureScript 1.9.946 {}
+goog.provide('hello_world.core');
+goog.require('cljs.core');
+cljs.core.enable_console_print_BANG_.call(null);
+cljs.core.println.call(null,"Hello world!");
+
+//# sourceMappingURL=core.js.map
